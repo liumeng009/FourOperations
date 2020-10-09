ca310a6ae50ba60074dbb6020a88e87a67e3ce06
liumeng009 <liu.meng.009@qq.com>
2020-10-09 11:10:44 +0800

    Update main.c

--- main.c
+++ main.c
@@ -1,63 +1,50 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 //全局变量
-int token;              //当前的token src永远指向下一个指针位置
+int token;              //当前的token
 int token_val;          //对应Num的value
 char * src = NULL;
 
-//代表数值类型的枚举  
-enum { Num = 128};      //区别于ASCII字符
+//缓冲区代表状态
+char exprInput[100];
 
-/*  输入：src永远指向未取用的位置
- *  
- *  批：一方面获得了token（主线） 另一方面也是向前推进 src指向下一位置
+enum { Num = 128};
+
+/*  next token 分两级判断  
 */
-void nextToken(){
+void next(){
 
-    while (*src == ' ' || *src == '\t')  { //小循环：去除所有空格和tab 一次搞完
+    while (*src == ' ' || *src == '\n'){
         src++;
     }
-    token = *src++;
-
+    
+    token = *src++;         //向前走一个，要根据token进行下一步判断
+        
     if (token >= '0' && token <= '9')
-    { //小循环: 识别数字串  一次搞完
-        token_val = token - '0';
-        token = Num;                //肯定是数字了
-
-        while (*src >= '0' && *src <= '9') //向前是数字：
-        {
-            token_val = token_val * 10 + *src - '0';
-            src++;
-        }
-        return;
-    }
-    else if (token == '(' || token == ')' || token == '+' || token == '-' || token == '*' || token == '/')
     {
-        return;
+        token_val = token - '0';
+        while (*src >= '0' && *src <= '9')
+        {
+            token = *src++;
+            token_val = token_val * 10 + token - '0';
+        }
+        token = Num;
+        return;     //主要是分支作用
     }
+
+    //如果是其他符号 直接使用token
+
+    //如果是结束 token == 0
 }
-/* match 获得token，然后与字面值进行比较, 如果相等，不用处理
+
+/* match(token) 消耗掉本token,next指向下一个token，如果结束token = 0
 */
 void match(int tk){
-    if(token != tk)
-        printf("different token! %c\n",tk);
+    if(token == tk)
+        next();
     else
-        nextToken();    
+        printf("different token! %c\n",tk);    
 }
-
-/*  BNF:代表着一种解释：用右边的格式套左边的数据，就像联合和结构体的共享内存关系, 每一路计算都必须经过叶子结点
-    <expr> ::= <term><expr_tail>
-    <expr_tail> ::= +<term><expr_tail>
-                  | -<term><expr_tail>
-                  | <empty>
-    <term> ::= <factor><term_tail>
-    <term_tail>  ::= *<factor><term_tail>
-                   | /<factor><term_tail>
-                   | <empty>
-    <factor> ::= Num
-                | (<expr>)
-*/
 
 int expr();
 int expr_tail(int lvalue);
@@ -67,21 +54,26 @@ int factor();
 
 /*这是第一个状态， 这个状态的返回结果就是，整个表达式的值*/
 int expr(){
-    int lvalue;         //进入expr状态后，就开始进行新的状态的创建（函数调用）和组织
+    int lvalue;     //进入expr状态后，就开始进行新的状态的创建（函数调用）和组织
     lvalue = term();    //创建一个新的状态，并获得其返回值
     return expr_tail(lvalue); //创建另外一个新的状态，返回其返回值
 }
 int expr_tail(int lvalue){
     int value;
+    int rvalue;
 
     if(token == '+'){            //如果向前看是+，此时token还不是+
         match('+');
-        value = lvalue + term();
+        rvalue = term();
+        value = lvalue + rvalue;
+        printf("%d + %d = %d\n",lvalue,rvalue,value);
         return expr_tail(value);
     }
     if(token == '-'){
         match('-');
-        value = lvalue - term();
+        rvalue = term();
+        value = lvalue - rvalue;
+        printf("%d + %d = %d\n",lvalue,rvalue,value);
         return expr_tail(value);
     }
     return lvalue;
@@ -92,56 +84,53 @@ int term(){
     return term_tail(lvalue);   //创建另一个新的状态
 }
 int term_tail(int lvalue){
-    int value = 0;
-    
-    if( token == '*'){
+    int value;
+    int rvalue;
+
+    if(token == '*'){
         match('*');
-        value = lvalue * factor();
+        rvalue = factor();
+        value = lvalue * rvalue;
+        printf("%d + %d = %d\n",lvalue,rvalue,value);
         return term_tail(value);
     }
-    if( token == '/'){
+    if(token == '/'){
         match('/');
-        value = lvalue / factor();
+        rvalue = factor();
+        value = lvalue / rvalue;
+        printf("%d + %d = %d\n",lvalue,rvalue,value);
         return term_tail(value);
     }
     return lvalue;
 }
 int factor(){
-    int value = 0;
-    //nextToken();     //获得src指向的token,src+1
+    int value;
 
     if(token == Num){
-        match(Num);                     //驱动向前走
+        match(Num);
         return token_val;
     }
     if(token == '('){
-        match('(');           //推进一个src
+        match('(');     //推进一个src
         value = expr();         //根据这个新src进行求值
         match(')');
         return value;
     }
 }
 
-/* 获得命令行的一个字符串，以EOF为结尾, 还是不会字符串的处理*/
-int getline(char * src){    
-    char ch;                            //保存单个字符的变量 
-    while ((ch = getchar()) && ch != 0 && ch != '\n'){
-        *src++ = ch;
-    }
-    *src = 0;           //结尾放入EOF，字符串构造完毕
-}
-
-
 int main(int argc, char * argv[]){
-    src = malloc(100);
 
-    while (getline(src))
-    {
-        printf("confirm  string  : %s\n", src);
-        nextToken();
-        printf("expr eval result : %d\n\n", expr());
-        printf("please input expr: ");
-    }
+    src = exprInput;
     
+    while(1){
+        printf("input expr string: ");
+        scanf("%s",src);
+        fflush(stdin);
+        printf("confirm: %s\n",src);
+        next();
+        printf("the result is : %d \n",expr());
+    }
+
+    getchar();
     return 0;
 }
